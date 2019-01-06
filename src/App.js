0d96a71088a3f83bc617c1c1987fb98095a62634
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-06 00:04:54 -0200

    A short list of what needs to be add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <ul>
+          <li>Create a Form and a Data base at Firebase</li>
+          <li>Link this form with the External Database</li>
+          <li>Create a List in another Page</li>
+          <li>Add: Remove and Edit Options</li>
+          <li>Style it using CSS/SASS</li>
+        </ul>
+        <p> List will be a class as well as ViewList. The rest may be a function.</p>
       </div>
     );
   }
