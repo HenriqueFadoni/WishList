67e57db10329f1f202d588b871a55490c576f3f3
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-07 09:49:07 -0200

    Managing changes in Form

--- src/container/Form/Form.js
+++ src/container/Form/Form.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
+    state = {
+        id: null,
+        title: '',
+        description: '',
+        submitted: false
+    };
+
     render () {
         return (
             <div className='Container'>
                 <div className='formContainer'>
                     <h1>Add a wish to your WishList!</h1>
                     <form>
-                        <input type="text" placeholder="Title" required/>
-                        <textarea placeholder="Description" required/>
+                        <input 
+                            type="text" 
+                            placeholder="Title" 
+                            onChange={event => this.setState({title: event.target.value})}
+                            required/>
+                        <textarea 
+                            placeholder="Description" 
+                            onChange={event => this.setState({description: event.target.value})}
+                            required/>
                         <button>Submit</button>
                     </form>
                 </div>
