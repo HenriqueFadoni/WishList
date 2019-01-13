5a9533b0fbbb6a58fdae3357f174e1c20e2612c4
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-13 20:31:26 -0200

    Getting Data From Database

--- src/container/WishList/WishList.js
+++ src/container/WishList/WishList.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import axios from '../../axios-setup';
 
 class WishList extends Component {
+    state = {
+        wishesArray: []
+    }
+
+    componentDidMount() {
+        axios.get('/wish.json')
+            .then(response => {
+                const wishes = Object.values(response.data);
+                this.setState({wishesArray: wishes});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+
+
     render () {
         return (
             <div>
-                <p>something</p>
+                <p>k</p>
             </div>
         );
     }
