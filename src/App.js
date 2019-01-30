7eaebd600d4205275276129ad51a81ae3d1348c2
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-30 16:36:32 -0200

    Handling User Signed in at Home Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, withRouter} from 'react-router-dom';
+import { connect } from "react-redux";
 
 import Intro from './component/Intro/Intro';
 import Authentication from './container/Authentication/Authentication';
 import WishRegister from './container/WishRegister/WishRegister';
 import WishList from './container/WishList/WishList';
 
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Route path="/" component={Intro} exact/>
-        <Route path="/Authentication" component={Authentication}/>
-        <Route path="/WishListRegister" component={WishRegister}/>
-        <Route path="/WishList" component={WishList}/>
+        <Route path="/" render={(propsRouter) => <Intro {...propsRouter} isSignIn={this.props.token} />} exact/>
+        <Route path="/Authentication" component={Authentication} />
+        <Route path="/WishListRegister" component={WishRegister} />
+        <Route path="/WishList" component={WishList} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    token: state.authSignIn.idToken
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App));
--- src/component/Intro/Intro.js
+++ src/component/Intro/Intro.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 import '../../sass/main.scss';
 
-const Intro = () => {
+const Intro = props => {
     return (
         <div className="main-div ">
             <h2 className="sub-title">Welcome To</h2>
             <h1 className="title">WISHLIST</h1>
             <NavLink to="/Authentication">
-                <button className="btn-sign-up">SIGN UP</button>
+                <button className="btn-sign-up">{props.isSignIn ? 'MAKE A WISH' : 'SIGN IN'}</button>
             </NavLink>
         </div>
     );
